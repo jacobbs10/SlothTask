0354849ecb0ee2d93c930a6601a286d54f083001
jacobbs10 <jacob.bensaid@gmail.com>
2025-06-30 14:16:13 +0300

    adding cors

--- server/server.js
+++ server/server.js
@@ -1,7 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const path = require('path');
 
 const app = express();
+
+app.use(cors());
+
 const port = process.env.PORT || 8000;
 
 app.use('/hls', express.static(path.join(__dirname, 'hls')));
